eaceb0e3e983af0b7ea9fa72c26b9aabf0467831
nnagar31 <nagar.nupur@gmail.com>
2022-06-20 22:38:15 +0200

    deleted some lines

--- test.R
+++ test.R
@@ -2,9 +2,5 @@ print("This file was created within RStudio")
 print("And now it lives on GitHub")
 print("is no work or what")
 
-#"sysvum-xoVhah-0jotda"
 
-#token   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
-
-
-wjjfkwjfkdnckjwefhv sksdf
+install.packages("rmarkdown")
